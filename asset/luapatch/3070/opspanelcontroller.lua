80db1cdcc2d3b866d38cd399a92cb62e63f90d44
ZeroRin <ZeroRin@users.noreply.github.com>
2025-06-10 14:47:17 +0000

    US | 30600 | data c4e0a2b | dabao 20250610_11_44

--- asset/luapatch/3070/opspanelcontroller.lua
+++ asset/luapatch/3070/opspanelcontroller.lua
@@ -22,8 +22,89 @@ local ShowItemLimitUINew = function(self,itemids)
 		end
 	end
 end
+spotshowinfo = {};
+local Load = function(self,campaion)
+	self:Load(campaion);
+	local textAsset = CS.ResManager.GetObjectByPath("ProfilesConfig/OPSPanel/Campion"..tostring(campaion), ".txt");
+	if textAsset == nil then
+		return;
+	end
+	spotshowinfo = {};
+	local lineTxt = Split(textAsset.text,"\n");
+	for i = 1, #lineTxt do
+		local index = string.find(lineTxt[i],"SpotSelectShow");
+		if index ~= nil then
+			local temp = Split(lineTxt[i],"|");
+			if temp[1] == "SpotSelectShow" then
+				local info = {};
+				info[1] = tonumber(temp[2]);
+				info[2] = temp[3];
+				table.insert(spotshowinfo,info);
+			end			
+		end
+	end
+end	
+
+local TriggerSelectOPSMissionBase = function(self,missionBase)
+	self:TriggerSelectOPSMissionBase(missionBase);
+	for i=1,#spotshowinfo do
+		local index = spotshowinfo[i][1];
+		local path = spotshowinfo[i][2];	
+		local trans = self.transform.parent:Find(path);
+		if trans == nil then
+			CS.NDebug.LogError("未找到组件",path);
+		else			
+			if missionBase ~= nil and missionBase.order == index then
+				--CS.NDebug.Log("激活",trans);
+				trans.gameObject:SetActive(true);
+			else
+				--CS.NDebug.Log("隐藏",trans);
+				trans.gameObject:SetActive(false);
+			end
+		end
+	end
+end
+
+local TriggerSelectOPSSpot = function(self,oPSPanelSpot)
+	self:TriggerSelectOPSSpot(oPSPanelSpot);
+	for i=1,#spotshowinfo do
+		local index = spotshowinfo[i][1];
+		local path = spotshowinfo[i][2];
+		local trans = self.transform.parent:Find(path);
+		if trans == nil then
+			CS.NDebug.LogError("未找到组件",path);
+		else
+			if oPSPanelSpot ~= nil and oPSPanelSpot.missionHolderOrder == index then
+				--CS.NDebug.Log("激活",trans);
+				trans.gameObject:SetActive(true);
+			else
+				--CS.NDebug.Log("隐藏",trans);
+				trans.gameObject:SetActive(false);
+			end
+		end
+	end
+end
+
+function Split(szFullString, szSeparator)
+	local nFindStartIndex = 1
+	local nSplitIndex = 1
+	local nSplitArray = {}
+	while true do
+		local nFindLastIndex = string.find(szFullString, szSeparator, nFindStartIndex)
+		if not nFindLastIndex then
+			nSplitArray[nSplitIndex] = string.sub(szFullString, nFindStartIndex, string.len(szFullString))
+			break
+		end
+		nSplitArray[nSplitIndex] = string.sub(szFullString, nFindStartIndex, nFindLastIndex - 1)
+		nFindStartIndex = nFindLastIndex + string.len(szSeparator)
+		nSplitIndex = nSplitIndex + 1
+	end
+	return nSplitArray
+end
 
 util.hotfix_ex(CS.OPSPanelController,'Start',Start)
 util.hotfix_ex(CS.OPSPanelController,'ShowItemLimitUINew',ShowItemLimitUINew)
-
+util.hotfix_ex(CS.OPSPanelController,'Load',Load)
+util.hotfix_ex(CS.OPSPanelController,'TriggerSelectOPSSpot',TriggerSelectOPSSpot)
+util.hotfix_ex(CS.OPSPanelController,'TriggerSelectOPSMissionBase',TriggerSelectOPSMissionBase)
 
